78af91d621fe07d2b63e718ad41c500b05626ba8
Arnau Diaz <diazarnau0@gmail.com>
2021-04-21 18:56:27 +0200

    huffman encoding for alphabet, tree decision, frequency based and iterative naives

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vrnvu/huffman
 
 go 1.15
+
+require github.com/disiqueira/gotree v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strings"
+
+	"github.com/disiqueira/gotree"
 )
 
 type ByLength struct {
@@ -21,6 +25,18 @@ type Strategy struct {
 	m    *Measures
 }
 
+type Node struct {
+	Letters    []rune  `json:"letters"`
+	Probabilty float64 `json:"probabilty"`
+	Children   []*Node `json:"children"`
+}
+
+type ByAscendingProbability []*Node
+
+func (a ByAscendingProbability) Len() int           { return len(a) }
+func (a ByAscendingProbability) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAscendingProbability) Less(i, j int) bool { return a[i].Probabilty < a[j].Probabilty }
+
 func newMeasures() *Measures {
 	return &Measures{
 		byLength: make(map[int]ByLength),
@@ -32,6 +48,29 @@ func (m *Measures) record(wordLength int, numQuestions int) {
 	byLength.numWords += 1
 	byLength.totalQuestions += int64(numQuestions)
 	m.byLength[wordLength] = byLength
+}
+
+func (n *Node) string() string {
+	return fmt.Sprintf("%q (%.2f%%)", string(n.Letters), n.Probabilty*100)
+}
+
+func makeTree(tree gotree.Tree, node *Node) {
+	for _, n := range node.Children {
+		t := tree.Add(n.string())
+		makeTree(t, n)
+	}
+}
+
+var rootNode *Node
+
+func init() {
+	bs, err := ioutil.ReadFile("huffman.json")
+	if err != nil {
+		panic(err)
+	}
+	var node Node
+	json.Unmarshal(bs, &node)
+	rootNode = &node
 }
 
 func readWords(fileName string) []string {
@@ -66,10 +105,71 @@ func strategy1(word string) int {
 	return numQuestions
 }
 
+func strategy2(word string) int {
+	frequencyLetter := "eiaonsrtlcupdmhgybfvkwxzqj"
+	numQuestions := 0
+	for _, c := range word {
+		for _, l := range frequencyLetter {
+			numQuestions += 1
+			if c == l {
+				break
+			}
+		}
+		numQuestions += 1
+	}
+	return numQuestions
+}
+
+func strategy3(word string) int {
+	return (5 + 1) * len(word)
+}
+
+func strategy4(word string) int {
+	// dummy tree
+	numQuestions := 0
+	for _, c := range word {
+		if c == 'i' || c == 'e' {
+			numQuestions += 2
+		} else {
+			numQuestions += 6
+		}
+		numQuestions += 1
+	}
+	return numQuestions
+}
+
+func strategy5(word string) int {
+	numQuestions := 0
+	for _, wordLetter := range word {
+		node := rootNode
+		for len(node.Letters) > 1 {
+		searchChild:
+			for _, child := range node.Children {
+				for _, nodeLetter := range child.Letters {
+					if nodeLetter == wordLetter {
+						node = child
+						break searchChild
+					}
+				}
+			}
+			// every time we travel to a new node, that's a question!
+			numQuestions++
+		}
+
+		// ask if the word is over
+		numQuestions++
+	}
+	return numQuestions
+}
+
 func getStrategies() []Strategy {
 	return []Strategy{
 		{name: "strategy0", f: strategy0, m: newMeasures()},
 		{name: "strategy1", f: strategy1, m: newMeasures()},
+		{name: "strategy2", f: strategy2, m: newMeasures()},
+		{name: "strategy3", f: strategy3, m: newMeasures()},
+		{name: "strategy4", f: strategy4, m: newMeasures()},
+		{name: "strategy5", f: strategy5, m: newMeasures()},
 	}
 }
 
@@ -113,28 +213,89 @@ func updateStrategiesMeasures(words []string, strategies []Strategy) {
 	}
 }
 
-func measureLetterFrequency(words []string) {
-	frequencies := make([]int64, 26)
+func measureLetterFrequency(words []string) []float64 {
+	frequenciesCount := make([]int64, 26)
+	frequencies := make([]float64, 26)
 	var total int64
+
 	for _, w := range words {
 		for _, c := range w {
-			frequencies[int(c-'a')] += 1
+			frequenciesCount[int(c-'a')] += 1
 			total += 1
 		}
 	}
 
+	for i := 0; i < 26; i++ {
+		frequencies[i] = float64(frequenciesCount[i]) / float64(total)
+
+	}
+
+	return frequencies
+}
+
+func printLetterFrequency(frequencies []float64) {
 	fmt.Printf("letter,frequency\n")
 	for i := 0; i < 26; i++ {
 		letter := 'a' + rune(i)
-		frequency := float64(frequencies[i]) / float64(total)
-		fmt.Printf("%v,%.5f\n", string([]rune{letter}), frequency)
+		fmt.Printf("%v,%.5f\n", string([]rune{letter}), frequencies[i])
 	}
+}
+
+func makeLeafsFromFrequencies(frequencies []float64) []*Node {
+	var leafs []*Node
+	for i := 0; i < 26; i++ {
+		letter := 'a' + rune(i)
+		frequency := frequencies[i]
+		leafs = append(leafs, &Node{
+			Letters:    []rune{letter},
+			Probabilty: frequency,
+			Children:   nil,
+		})
+	}
+	return leafs
+}
+
+func buildRecursivelySortedComposedNodes(leafs []*Node) []*Node {
+	for len(leafs) > 1 {
+		sort.Sort(ByAscendingProbability(leafs))
+		l := leafs[0]
+		r := leafs[1]
+		newNode := &Node{
+			Letters:    append(l.Letters, r.Letters...),
+			Probabilty: l.Probabilty + r.Probabilty,
+			Children:   []*Node{l, r},
+		}
+		leafs = append([]*Node{newNode}, leafs[2:]...)
+	}
+	return leafs
+}
+
+func buildFrequencyDecissionTree(leafs []*Node) gotree.Tree {
+	tree := gotree.New(leafs[0].string())
+	makeTree(tree, leafs[0])
+	return tree
+}
+
+func serializeFrequencyDecissionTree(fileName string, leafs []*Node) {
+	bs, _ := json.MarshalIndent(leafs[0], "", "")
+	_ = ioutil.WriteFile(fileName, bs, 0644)
+	fmt.Println("serialized to " + fileName)
 }
 
 func main() {
 	words := readWords("words.txt")
-	// strategies := getStrategies()
-	// updateStrategiesMeasures(words, strategies)
-	// printStrategiesCsv(strategies)
-	measureLetterFrequency(words)
+	strategies := getStrategies()
+	updateStrategiesMeasures(words, strategies)
+	printStrategiesCsv(strategies)
+
+	// frequencies := measureLetterFrequency(words)
+	// printLetterFrequency(frequencies)
+
+	// leafs := makeLeafsFromFrequencies(frequencies)
+	// leafs = buildRecursivelySortedComposedNodes(leafs)
+
+	// tree := buildFrequencyDecissionTree(leafs)
+	// fmt.Println(tree.Print())
+
+	// serializeFrequencyDecissionTree("huffman.json", leafs)
 }
